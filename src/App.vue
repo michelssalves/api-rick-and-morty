75433472992547a0f5ae072fc7d6d468bf87c4a4
Michel <michelssalves@gmail.com>
2022-12-13 14:25:06 -0300

    2022-12-13 14:2511:55

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Character</router-link>
+      <router-link to="/location">Location</router-link>
+      <router-link to="/episodes">Episode</router-link> 
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Character from '../views/Character.vue'
+import Location from '../views/Location.vue'
+import Episodes from '../views/Episodes.vue'
 
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
+
+  {path: '/', name: 'Character', component: Character},
+  {path: '/location', name: 'Location', component: Location},
+  {path: '/episodes', name: 'Episodes', component: Episodes},
 ]
 
 const router = new VueRouter({

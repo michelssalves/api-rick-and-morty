cbad3d1ef8f01381850aa087538b2538661520ea
Michel <michelssalves@gmail.com>
2022-12-13 11:55:19 -0300

    2022-12-13 11:55

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+      <h1>Cabeçalho</h1>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'Home',
+    components: {
+  
+    },
+
+  }
+  </script>
+  <style>
+  .home {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .size-img {
+      width: 75px;
+      height: 75px;
+  }
+  </style>
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,39 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Ta funcionando</h1>
     <div class="container">
+      <input type="text" v-model="search" placeholder="Search">
       <table class="table table-dark">
         <thead>
           <tr>
-            <th scope="col">Image</th>
-            <th scope="col">Name</th>
-            <th scope="col">Gender</th>
+            <th>Id</th>
+            <th>Image</th>
+            <th>Location</th>
+            <th>Episode</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="character in characters">
-            <th ><img class="size-img" :src="character.image"/></th>
-            <th>{{ character.name }}</th>
-            <th>{{ character.gender }}</th>
-            
+          <tr>
+            <th>
+              <li v-for="character in characters">{{ character.id}}</li>
+            </th>
+            <th>
+              <ul>
+                <li v-for="character in characters" :key="character.id"><img class="size-img" :src="character.image"/></li>
+              </ul>
+            </th> 
+            <th>
+              <ul>
+                <li v-for="character in characters">{{ character.location.name }}</li>
+               
+               
+              </ul>
+            </th>
+            <th >
+              <ul>
+                <li v-for="character in characters">{{ character.episode }}</li>
+              </ul>
+            </th>
           </tr>
-
         </tbody>
       </table>
     </div>
@@ -26,6 +43,7 @@
 </template>
 <script>
 import axios from 'axios'
+
 export default {
   name: 'Home',
   components: {
@@ -33,17 +51,25 @@ export default {
   },
   data() {
     return {
-
+      x: 0,
+      search: '',
       characters: [],
+      locations: [],
+      episodes: []
+
 
     }
   },
   mounted() {
-    this.getAll()
+
+    this.getAllCharacters()
+    this.getAllEpisodes()
+    this.getAllLocations()
+
   },
   methods: {
 
-    async getAll() {
+    async getAllCharacters() {
       axios.get('https://rickandmortyapi.com/api/character')
         .then(res => {
           // manipula o sucesso da requisição
@@ -53,8 +79,41 @@ export default {
         })
         .catch(e => {
           // manipula erros da requisição
-          console.error(e);
+          //console.error(e);
         })
+    },
+    async getAllLocations() {
+      axios.get('https://rickandmortyapi.com/api/location')
+        .then(res => {
+          // manipula o sucesso da requisição
+          //console.log(res.data.results);
+          this.locations = res.data.results
+
+        })
+        .catch(e => {
+          // manipula erros da requisição
+         // console.error(e);
+        })
+    },
+    async getAllEpisodes() {
+      axios.get('https://rickandmortyapi.com/api/episode')
+        .then(res => {
+          // manipula o sucesso da requisição
+         // console.log(res.data.results);
+          this.episodes = res.data.results
+
+        })
+        .catch(e => {
+          // manipula erros da requisição
+        //  console.error(e);
+        })
+    }
+  },
+  computed: {
+    filterSearch(){
+      return this.characters.filter((character) => {
+        return character.name.toLowerCase().includes(this.search.toLowerCase())
+      })
     }
   }
 }
